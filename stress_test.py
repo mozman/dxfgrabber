80cd83f1b94c212dcf5f33a7dbc9184f393ba58b
Phillip Stromberg <phillip+dxfgrabber@stromberg.me>
2015-08-24 20:29:44 -0500

    Added __str__ and __repr__ functions to all objects in entities.py for easier debugging. Added a test for these new functions. stress_test.py checks to see if it is __main__ so the new test can use its all_files variable to find the DXF test files.

--- stress_test.py
+++ stress_test.py
@@ -15,27 +15,34 @@ else:
     "/media/sf_winsource/dxftest/R12_test_files/*.dxf")
 
 all_files = chain(*[glob.glob(d)for d in DIRS])
-overall_time = 0
-ok = 0
-error = 0
 
-print('Cython acceleration is ' + ('on' if CYTHON_EXT else 'off'))
-print("Stress test:")
 
-for filename in all_files:
-    try:
-        start_time = time.time()
-        dwg = dxfgrabber.readfile(filename)
-        end_time = time.time()
-        run_time = end_time - start_time
-    except Exception:
-        error += 1
-        print('errors at opening: {}'.format(filename))
-    else:
-        ok += 1
-        overall_time += run_time
-        print('ok ({:.2f} seconds for opening: {})'.format(run_time, filename))
+def stress_test():
+    overall_time = 0
+    ok = 0
+    error = 0
 
-print("{:.2f} seconds for {} files".format(overall_time, ok))
-print("{} errors".format(error))
-print('success' if error == 0 else 'failed')
+    print('Cython acceleration is ' + ('on' if CYTHON_EXT else 'off'))
+    print("Stress test:")
+
+    for filename in all_files:
+        try:
+            start_time = time.time()
+            dwg = dxfgrabber.readfile(filename)
+            end_time = time.time()
+            run_time = end_time - start_time
+        except Exception:
+            error += 1
+            print('errors at opening: {}'.format(filename))
+        else:
+            ok += 1
+            overall_time += run_time
+            print('ok ({:.2f} seconds for opening: {})'.format(run_time, filename))
+
+    print("{:.2f} seconds for {} files".format(overall_time, ok))
+    print("{} errors".format(error))
+    print('success' if error == 0 else 'failed')
+
+
+if __name__ == "__main__":
+    stress_test()
---
+++ tests/test_entity_repr.py
@@ -0,0 +1,44 @@
+# Created: 17.08.15
+# License: MIT License
+from __future__ import unicode_literals
+import traceback
+import sys
+
+__author__ = "phistrom <phillip+dxfgrabber@stromberg.me>"
+
+import dxfgrabber
+import os
+from stress_test import all_files
+import unittest
+import tempfile
+
+
+
+class TestEntityRepr(unittest.TestCase):
+    """
+    Tests to make sure all classes in the entities module do not raise an exception when converted to a human-readable
+    string. This generates an approximately 38MB text file when using the CADKit sample DXFs.
+    """
+    def test_entity_repr(self):
+        with tempfile.NamedTemporaryFile(suffix='.txt', delete=False) as outfile:
+            print("Writing all entities as human-readable strings to %s" % outfile.name)
+            for filename in all_files:
+                print("Printing all entities in %s to %s" % (filename, outfile.name))
+                self.print_all_entity_reprs(filename, outfile)
+            print("Finished printing entities")
+        os.remove(outfile.name)  # deletes the temporary file manually at the end so you can examine the output
+
+    @staticmethod
+    def print_all_entity_reprs(filename, output):
+        dxf = dxfgrabber.readfile(filename)
+        all_entities = [entity for entity in dxf.entities]
+        for block in dxf.blocks:
+            for entity in block:
+                all_entities.append(entity)
+        for e in all_entities:
+            if sys.version_info[0] == 2:
+                value = str(e).decode('utf-8') + "\t" + repr(e).decode('utf-8')
+            else:
+                value = str(e) + "\t" + repr(e)
+            output.write(value.encode('utf-8'))
+            output.write(os.linesep.encode())
